8c8cccdf49d191fd7a8b5e824fac952bdd243399
AhmedKhalaf-97 <ahmedkhalafx97@gmail.com>
2024-06-23 06:49:03 -0700

    Error handling & Alerts

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import leads from "./leads";
+import errors from "./errors";
+import messages from "./messages";
 
 export default combineReducers({
   leads,
+  errors,
+  messages,
 });
